b29374cb80fccf5de281214740fffd945350cf1a
Hashmatullah Nazari <hn0153@gmail.com>
2018-04-10 13:49:01 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".btn").click(function(){
+        $("ul").slideToggle(500);
+    });
+});
+
+$(window).resize(function(){
+    if(window.innerWidth > 700) {
+        $("ul").removeAttr("style");
+    }
+});
+
